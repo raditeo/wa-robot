a23f2f04aecba2e268bf433a93765942c5354a91
raditeo <raditeo@gmail.com>
2022-08-02 18:21:40 +0700

    update packages

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const qrcode = require('qrcode-terminal');
+const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
+const express = require('express');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {
+        root: __dirname
+    });
+});
+
 const client = new Client({
     restartOnAuthFail: true,
     puppeteer: {
@@ -19,12 +39,47 @@ const client = new Client({
     authStrategy: new LocalAuth()
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on('message', async msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
 });
 
 client.initialize();
+
+io.on('connection', function(socket) {
+    socket.emit('message', 'Connecting...');
+  
+    client.on('qr', (qr) => {
+      console.log('QR RECEIVED', qr);
+      qrcode.toDataURL(qr, (err, url) => {
+        socket.emit('qr', url);
+        socket.emit('message', 'QR Code received, scan please!');
+      });
+    });
+  
+    client.on('ready', () => {
+      socket.emit('ready', 'Whatsapp is ready!');
+      socket.emit('message', 'Whatsapp is ready!');
+    });
+  
+    client.on('authenticated', () => {
+      socket.emit('authenticated', 'Whatsapp is authenticated!');
+      socket.emit('message', 'Whatsapp is authenticated!');
+      console.log('AUTHENTICATED');
+    });
+  
+    client.on('auth_failure', function(session) {
+      socket.emit('message', 'Auth failure, restarting...');
+    });
+  
+    client.on('disconnected', (reason) => {
+      socket.emit('message', 'Whatsapp is disconnected!');
+      client.destroy();
+      client.initialize();
+    });
+  });
+
+  server.listen(port, function() {
+    console.log('App running on *: ' + port);
+  });

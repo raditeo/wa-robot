2ebab4901125b65846c39a74c21139f44d88c40c
raditeo <raditeo@gmail.com>
2022-08-02 18:58:17 +0700

    Googleapis

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
+const { google } = require('googleapis');
 
 const port = process.env.PORT || 8000;
 
@@ -39,9 +40,160 @@ const client = new Client({
     authStrategy: new LocalAuth()
 });
 
+// Settings start here
+const maintenaceMode = false;
+
+// Settings end here
+
+// Google Spreadsheet start here
+const privatekey = require("./privatekey.json");
+
+const authClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets.readonly']);
+
+
+// authentication
+authClient.authorize()
+  .then(function (tokens) {
+      console.log("Google API authentication successful.\n");
+  })
+  .catch(function (error) {
+      throw (error);
+  });
+
+const secrets = require("./secrets.json");
+const sheets = google.sheets('v4');
+
+async function readSpreadsheet(spreadsheetID, sheetName) {
+  try {
+    var result = await sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetID,
+        range: sheetName,
+        auth: authClient
+    });
+        
+    return result.data.values;
+  } catch (err) {
+    return [];
+  }
+}
+// Google Spreadsheet end here
+
 client.on('message', async msg => {
-    if (msg.body == '!ping') {
-        msg.reply('pong');
+    if (maintenaceMode) {
+        msg.reply("Maaf saat ini sedang dilakukan pemeliharaan sistem. Silakan kembali lagi nanti.");
+    } else {
+        const today = new Date();
+        const contact = await msg.getContact();
+        const phNumber = contact.number;
+
+        const bodyMsg = (msg.body).toLowerCase().trim().replace(/\s\s+/g, " ");
+        const bodyMsgSplit = bodyMsg.split(" ");
+
+        let greeting = "Selamat datang bapak/ibu.";
+        let message = "";
+
+        readSpreadsheet(secrets.spreadsheet_id_informasi, "Pesan").then((response) => {
+        if (response.length > 0) { 
+            const rows = response; 
+            let matchKey = false;
+            rows.forEach((element, index) => {
+                if (index > 0) {
+                    if (element[0] == "greeting") {
+                        greeting = element[1];
+                    }
+
+                    if (element[0] == bodyMsg) {
+                        message = element[1];
+                        matchKey = true;
+                    }
+                }
+            });
+
+            if (!matchKey) {
+                message = greeting;
+
+                if (bodyMsgSplit.length > 1) {
+                    if (bodyMsgSplit[0] == "sampah") { // iuran sampah
+                        const sheet = bodyMsgSplit[1];        
+                        let  message = `Tidak ada data iuran sampah.`;
+                
+                        if (sheet == "rekap") {
+                            msg.reply(message);
+                        } else {
+                            const tahun = sheet;
+                
+                            readSpreadsheet(secrets.spreadsheet_id_sampah, sheet).then((response) => {
+                                if (response.length > 0) {
+                                    const rows = response;
+                                    let  elementH = [];
+                                    let  elementD = [];
+                                    
+                                    rows.forEach((element,index) => {
+                                        if (index == 0) {
+                                            elementH = element; // ambil header sebagai label  
+                                        }
+                                
+                                        if (index > 0 && element[0] == phNumber) {
+                                            elementD = element; // ambil data sebagai value
+                                
+                                            if (elementD.length > 0) {
+                                            if (elementD.length > 3) {
+                                                message = "";
+                        
+                                                elementH.forEach((rowH, idH) => {
+                                                elementD.forEach((rowD, idD) => {
+                                                    if (idH == idD) {                            
+                                                    message = message.concat(rowH).concat(" = ").concat(rowD).concat("\r\n");
+                                                    }                
+                                                });
+                                                });
+                                            }
+                                            }                           
+                                        }          
+                                    });
+                            
+                                    msg.reply(message);
+                                } else {                
+                                    msg.reply(message);
+                                }          
+                            });
+                        }
+                    } else {
+                    readSpreadsheet(secrets.spreadsheet_id_informasi, "Pesan").then((response) => {
+                        if (response.length > 0) {
+                        const rows = response;
+                        rows.forEach((element, index) => {            
+                            if (index > 0 && element[0] == "greeting") {
+                            greeting = element[1];
+                            }
+                        });
+            
+                        message = greeting;
+                    
+                        msg.reply(message);
+                        } else {
+                        message = greeting;
+                    
+                        msg.reply(message);
+                        }
+                    });
+                    }
+                } else {
+                    msg.reply(message);
+                }
+            } else {
+                msg.reply(message);
+            }
+        } else {
+            message = greeting;
+        
+            msg.reply(message);
+        }
+        });    
     }
 });
 
